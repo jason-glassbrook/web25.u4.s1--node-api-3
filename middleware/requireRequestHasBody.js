8756a9a41e986362f8b655a0072cce6524a1b046
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 18:09:53 -0500

    rewrite requireRequestHasBody using requireRequestHas

--- middleware/requireRequestHasBody.js
+++ middleware/requireRequestHasBody.js
@@ -5,29 +5,15 @@
   -> here -X
 ***********************************************************/
 
-const _ = require ('lodash/fp')
-const { not } = require ('../tools')
-const respondWithError = require ('./respondWithError')
+const requireRequestHas = require ('./requireRequestHas')
 
 /***************************************
   definition
 ***************************************/
 
-const requireRequestHasBody = (...rest) => (ri, ro, next) => {
-  const isValid = _.conforms ({
-    'body' : _.isObject,
-  })
-
-  if (not (isValid (ri))) {
-    respondWithError (400,
-      `-- request must have a body`,
-      ...rest,
-    ) (ri, ro)
-  }
-  else {
-    next ()
-  }
-}
+const requireRequestHasBody = (...rest) => (
+  requireRequestHas ([ 'doorknob' ], ...rest)
+)
 
 /**************************************/
 

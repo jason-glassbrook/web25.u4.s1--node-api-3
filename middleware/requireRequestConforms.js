870fc8d2e7d1d43ce311c10e03e25d052c88979c
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 18:46:43 -0500

    write /middleware/requireRequestConforms

---
+++ middleware/requireRequestConforms.js
@@ -0,0 +1,36 @@
+/***********************************************************
+  requireRequestConforms
+------------------------------------------------------------
+  -> here -> next
+  -> here -X
+***********************************************************/
+
+const _ = require ('lodash/fp')
+const { not } = require ('../tools')
+const respondWithError = require ('./respondWithError')
+
+/***************************************
+  definition
+***************************************/
+
+const requireRequestConforms = (shape, customizer, restOfErrorMessage = '', restOfError = {}) => (ri, ro, next) => {
+  const requestConforms = _.conforms (shape) (ri)
+
+  if (not (requestConforms)) {
+    respondWithError (
+      400,
+      ` -- request must conform with <shape>` + restOfErrorMessage,
+      {
+        shape : 'please read the documentation',
+        ...restOfError,
+      },
+    ) (ri, ro)
+  }
+  else {
+    next ()
+  }
+}
+
+/**************************************/
+
+module.exports = requireRequestConforms

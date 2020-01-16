e7bb7fc05e895a18e165c32206c5ce081233cdc5
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 11:26:35 -0500

    rewrite logger to use timestamp if defined

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,4 +1,10 @@
+const _ = require ('lodash/fp')
+
 const logger = (ri, ro, next) => {
+  const terms = _.remove (_.isNil) ([ ri.method, ri.originalUrl, ri.timestamp ])
+
+  console.log (`--- ${_.join (' - ') (terms)} ---`)
+
   next ()
 }
 

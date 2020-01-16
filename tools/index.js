a307b2161c771a9a5e5c0ba86c243e26417c20b0
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 13:25:57 -0500

    add {has pick}Paths to /tools

--- tools/index.js
+++ tools/index.js
@@ -5,6 +5,8 @@ module.exports = {
   logInit : require ('./logInit'),
   logExit : require ('./logExit'),
   logEnds : require ('./logEnds'),
+  hasPaths : require ('./hasPaths'),
+  pickPaths : require ('./pickPaths'),
   hasShapeOf : require ('./hasShapeOf'),
   pickShapeOf : require ('./pickShapeOf'),
 }

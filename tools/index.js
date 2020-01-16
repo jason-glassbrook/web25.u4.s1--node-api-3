82d0916d077b53ea22a9b12d856091ec3e7bc05d
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 13:07:47 -0500

    add logging funs to /tools

---
+++ tools/index.js
@@ -0,0 +1,8 @@
+/**************************************/
+
+module.exports = {
+  log : require ('./log'),
+  logInit : require ('./logInit'),
+  logExit : require ('./logExit'),
+  logEnds : require ('./logEnds'),
+}

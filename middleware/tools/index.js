d07fae613a02318807d51e3ecf77c74207746816
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 12:22:07 -0500

    break tools/index.js into own files

--- middleware/tools/index.js
+++ middleware/tools/index.js
@@ -1,24 +1,7 @@
-/***************************************
-  tools
-***************************************/
-
-const kvPairs = (pairs, rest = {}) => ({
-  ...rest,
-  ...Object.fromEntries (pairs),
-})
-
-const message = (text, rest = {}) => kvPairs (
-  [[ 'message', text ]], rest
-)
-
-const error = (text, rest = {}) => kvPairs (
-  [[ 'error', message (text, rest) ]]
-)
-
 /**************************************/
 
 module.exports = {
-  kvPairs,
-  message,
-  error,
+  kvPairs : require ('./kvPairs'),
+  message : require ('./message'),
+  error : require ('./error'),
 }

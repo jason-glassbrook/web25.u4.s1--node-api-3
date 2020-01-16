e97e185c941f76f1987a1357f529f4058aba4d7c
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-16 13:41:37 -0500

    rewrite pickShapeOf to use pickPaths

--- tools/pickShapeOf.js
+++ tools/pickShapeOf.js
@@ -3,19 +3,26 @@
 ***********************************************************/
 
 const _ = require ('lodash/fp')
-// const log = require ('./log')
+const pickPaths = require ('./pickPaths')
 
 /***************************************
   definition
 ***************************************/
 
-const pickShapeOf = (shape) => (
+const pickShapeOf = /* (shape) => (object) => */ (
   _.flow ([
-    // log ('- before:'),
-    _.pick (shape),
-    // log ('- after:'),
-  ])
+    _.keys,
+    pickPaths,
+  ]) /* (shape) (object) */
 )
+
+// const old.pickShapeOf = (shape) => /* (object) => */ (
+//   _.flow ([
+//     // log ('- before:'),
+//     _.pick (shape),
+//     // log ('- after:'),
+//   ]) /* (object) */
+// )
 
 /**************************************/
 

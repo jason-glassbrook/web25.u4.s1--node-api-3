fba3962eec20a54459c4355e46ccdf2cc298f1d6
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-15 21:05:21 -0500

    rewrite /res/o with express-js cb signature

--- res/o/index.js
+++ res/o/index.js
@@ -6,9 +6,10 @@ const r = require ('../')
 ***************************************/
 
 const messages = {
-  'hello' : (path) => (r.message (
-    `hello! i'm ~${path}.`
-  )),
+  'hello' :
+    () => (ri, ro) => (r.message (
+      `hello! i'm ~${ri.path}.`
+    )),
 }
 
 /***************************************
@@ -16,18 +17,22 @@ const messages = {
 ***************************************/
 
 const errors = {
-  '400' : (request, thing) => (r.error (
-    `bad request to ${request} ${thing}`,
-  )),
-  '404' : (thing, id) => (r.error (
-    `could not find ${thing} with id ${id}`,
-  )),
-  '500' : () => (r.error (
-    `something bad happened`,
-  )),
-  '501' : (request, path) => (r.error (
-    `${request} @ ${path} not implemented ... yet?`,
-  )),
+  '400' :
+    (thing) => (ri, ro) => (r.error (
+      `bad request to ${request} ${thing}`,
+    )),
+  '404' :
+    (thing, id) => (ri, ro) => (r.error (
+      `could not find ${thing} with id ${id}`,
+    )),
+  '500' :
+    () => (ri, ro) => (r.error (
+      `something bad happened`,
+    )),
+  '501' :
+    () => (ri, ro) => (r.error (
+      `${ri.method} @ ${ri.path} not implemented ... yet?`,
+    )),
 }
 
 /**************************************/
